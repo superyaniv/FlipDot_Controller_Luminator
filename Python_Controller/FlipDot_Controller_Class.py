3f4e113d20dc90766c3bff96300ce12c9471f138
Yaniv Alfasy <yanivalfasy@Yanivs-MBP.lan>
2017-12-27 02:53:55 -0800

    updating updateDisplay.

--- Python_Controller/FlipDot_Controller_Class.py
+++ Python_Controller/FlipDot_Controller_Class.py
@@ -113,15 +113,15 @@ class FlipDot_Controller_Class:
 		for segment in range(self.displaySegments):
 			for segment_column in range(self.columns_per_Segment-1):
 				for row in range(self.displayRows):
-					if self.currentDisplayState[segment][segment_column][row] and not self.oldDisplayState[segment][segment_column-column_offset][row]:
-						self.registers[self.onColumns[segment*self.columns_per_Segment+segment_column-column_offset]]=1
+					if self.currentDisplayState[segment][segment_column+column_offset][row] and not self.oldDisplayState[segment][segment_column][row]:
+						self.registers[self.onColumns[segment*self.columns_per_Segment+segment_column+column_offset]]=1
 						self.registers[self.onRows[row]]=1
 				self.writeRegisters()
 				sleep(self.flipDelay)
 				self.clearRegisters()
 				for row in range(self.displayRows):
-					if not self.currentDisplayState[segment][segment_column][row] and self.oldDisplayState[segment][segment_column-column_offset][row]:
-						self.registers[self.offColumns[segment*self.columns_per_Segment+segment_column-column_offset]]=1
+					if not self.currentDisplayState[segment][segment_column+column_offset][row] and self.oldDisplayState[segment][segment_column][row]:
+						self.registers[self.offColumns[segment*self.columns_per_Segment+segment_column+column_offset]]=1
 						self.registers[self.offRows[row]]=1
 					self.oldDisplayState[segment][segment_column][row] = self.currentDisplayState[segment][segment_column][row]
 				self.writeRegisters()
