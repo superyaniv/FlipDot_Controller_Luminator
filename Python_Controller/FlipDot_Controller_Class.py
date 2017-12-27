135f2a1e99f90781cdc25b3d498155ac857cd60e
Yaniv Alfasy <yanivalfasy@Yanivs-MBP.lan>
2017-12-26 18:15:56 -0800

    added state history to controller class.

--- Python_Controller/FlipDot_Controller_Class.py
+++ Python_Controller/FlipDot_Controller_Class.py
@@ -78,10 +78,9 @@ class FlipDot_Controller_Class:
 		for segment in range(self.displaySegments):
 			for segment_column in range(self.columns_per_Segment-1):
 				for row in range(self.displayRows):
-					if self.oldDisplayState[segment][segment_column][row] or self.oldDisplayState[segment][segment_column][row] is None:
-						self.registers[self.offColumns[segment*self.columns_per_Segment+segment_column]]=1
-						self.registers[self.offRows[row]]=1
-					self.oldDisplayState[segment][segment_column][row] = 0
+					self.registers[self.offColumns[segment*self.columns_per_Segment+segment_column]]=1
+					self.registers[self.offRows[row]]=1
+					self.oldDisplayState[segment][segment_column][row]=0
 
 	def updateDisplay(self, textMessage):
 		alphabetIndex = []
