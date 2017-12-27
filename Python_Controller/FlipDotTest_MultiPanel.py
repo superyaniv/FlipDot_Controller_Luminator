13c4d2d9dbb155275dfcf4c515d70f648cdd3415
Yaniv Alfasy <yanivalfasy@Yanivs-MBP.lan>
2017-12-27 08:27:22 -0800

    updating multipanel threading

--- Python_Controller/FlipDotTest_MultiPanel.py
+++ Python_Controller/FlipDotTest_MultiPanel.py
@@ -13,7 +13,7 @@ offRows = [8,9,10,11,12,13,14] #[53,61,56,60,57,59,58]
 onColumns = [31,30,29,28,27,24,25,26,72,40,41,42,43,44,45,46,47,56,57,58,73,74,75,76,77,63,62,61,60,59] #[54,52,51,50,49,48,47,46,45,55,62,63,64,65,66,67,68,69,70,71,44,43,42,41,40,76,75,74,73,72]
 offColumns = [23,22,21,20,19,16,17,18,64,32,33,34,35,36,37,38,39,48,49,50,65,66,67,68,69,55,54,53,52,51] #[10,12,13,14,15,8,1,2,3,9,18,17,24,31,30,29,28,27,26,25,4,5,6,7,0,36,37,38,39,32]
 numOfRegisterPins = 10 * 8 
-FlipDot_Panels = [0 for i in range(1)]
+FlipDot_Panels = [0 for i in range(2)]
 
 #----Panel #1----#
 ser_Pin = 18
@@ -21,10 +21,10 @@ rclk_Pin = 23
 srclk_Pin = 24
 FlipDot_Panels[0] = FlipDot_Controller_Class.FlipDot_Controller_Class(1, onRows, offRows, onColumns, offColumns, numOfRegisterPins, ser_Pin, rclk_Pin, srclk_Pin) 
 #----Panel #2----#
-#ser_Pin = 18
-#rclk_Pin = 23
-#srclk_Pin = 24
-#FlipDot_Panels[2] = FlipDot_Controller_Class.FlipDot_Controller_Class(1, onRows, offRows, onColumns, offColumns, numOfRegisterPins, ser_Pin, rclk_Pin, srclk_Pin) 
+ser_Pin = 17
+rclk_Pin = 27
+srclk_Pin = 22
+FlipDot_Panels[1] = FlipDot_Controller_Class.FlipDot_Controller_Class(1, onRows, offRows, onColumns, offColumns, numOfRegisterPins, ser_Pin, rclk_Pin, srclk_Pin) 
 #----Panel #3----#
 #ser_Pin = 18
 #rclk_Pin = 23
@@ -64,7 +64,8 @@ def displayScroller():
 		pass
 
 def worker(panelNumber,panelDisplay,columns_offset_total):
-	FlipDot_Panels[panelNumber].updateDisplay(panelDisplay,columns_offset_total)
+	if panelNumber ==0 then:
+		FlipDot_Panels[panelNumber].updateDisplay(panelDisplay,columns_offset_total)
 
 
 displayScroller()
