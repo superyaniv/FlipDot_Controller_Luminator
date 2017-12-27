587f1cecadaf4335d7f2fc6b5482ba38a113562e
Yaniv Alfasy <yanivalfasy@Yanivs-MBP.lan>
2017-12-27 02:35:31 -0800

    updating updateDisplay.

--- Python_Controller/FlipDot_Controller_Class.py
+++ Python_Controller/FlipDot_Controller_Class.py
@@ -88,7 +88,7 @@ class FlipDot_Controller_Class:
 	def updateDisplay(self, textMessage):
 		alphabetIndex = []
 		currentColumn = 0
-		self.currentDisplayState = [[[0 for s in range(displayRows)] for c in range(columns_per_Segment)] for r in range(len(textMessage))]
+		self.currentDisplayState = [[[0 for s in range(self.displayRows)] for c in range(self.columns_per_Segment)] for r in range(len(textMessage))]
 
 		for ch in range(len(textMessage)):
 			alphabetIndex.append(ord(textMessage[ch])-32)
