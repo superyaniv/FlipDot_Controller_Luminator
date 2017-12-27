9e0ee50e4d85957567ccde354787ac2f48081711
Yaniv Alfasy <yanivalfasy@Yanivs-MBP.lan>
2017-12-26 19:10:07 -0800

    updating scroller test.

--- Python_Controller/FlipDotTest_Scroller.py
+++ Python_Controller/FlipDotTest_Scroller.py
@@ -23,7 +23,7 @@ def displayScroller():
 		print "Press Ctrl+C to Stop Test."
 		while True:
 			FlipDot_Controller.updateDisplay(t[p:]+t)
-			if p>len(t)-5:
+			if p>len(t)-4:
 				p=0
 			else:
 				p=p+1
