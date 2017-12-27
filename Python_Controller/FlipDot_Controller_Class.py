9fca7f863fd2d060db5fb2ce28492a8e5f52737b
Yaniv Alfasy <yanivalfasy@Yanivs-MBP.lan>
2017-12-27 02:40:47 -0800

    updating updateDisplay.

--- Python_Controller/FlipDot_Controller_Class.py
+++ Python_Controller/FlipDot_Controller_Class.py
@@ -85,7 +85,7 @@ class FlipDot_Controller_Class:
 				sleep(self.flipDelay)
 				self.clearRegisters()
 
-	def updateDisplay(self, textMessage):
+	def updateDisplay(self, textMessage, column_offset=0, row_offset=0):
 		alphabetIndex = []
 		currentColumn = 0
 		self.currentDisplayState = [[[0 for s in range(self.displayRows)] for c in range(self.columns_per_Segment)] for r in range(len(textMessage))]
@@ -97,6 +97,8 @@ class FlipDot_Controller_Class:
 
 		for segment in range(len(textMessage)):
 			for segment_column in range(self.columns_per_Segment-1):
+				if segment*self.columns_perSegment+segment_column < offset:
+					break
 				columnbins= self.alphabet[alphabetIndex[segment]]
 				x = bin(columnbins[segment_column])
 				
