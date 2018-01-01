37e6719a2a0a50f6991b41be2e842ffc6ab8e271
Yaniv Alfasy <yanivalfasy@Yanivs-MBP.lan>
2017-12-31 16:13:32 -0800

    updating secondary pin layout

--- Python_Controller/FlipDotTest_MultiPanel.py
+++ Python_Controller/FlipDotTest_MultiPanel.py
@@ -13,7 +13,7 @@ offRows = [53,61,56,60,57,59,58] #[8,9,10,11,12,13,14]
 onColumns = [54,52,51,50,49,48,47,46,45,55,62,63,64,65,66,67,68,69,70,71,44,43,42,41,40,76,75,74,73,72] #[31,30,29,28,27,24,25,26,72,40,41,42,43,44,45,46,47,56,57,58,73,74,75,76,77,63,62,61,60,59]
 offColumns = [10,12,13,14,15,8,1,2,3,9,18,17,24,31,30,29,28,27,26,25,4,5,6,7,0,36,37,38,39,32] #[23,22,21,20,19,16,17,18,64,32,33,34,35,36,37,38,39,48,49,50,65,66,67,68,69,55,54,53,52,51]
 numOfRegisterPins = 10 * 8 
-FlipDot_Panels = [0 for i in range(3)]
+FlipDot_Panels = [0 for i in range(2)]
 
 #----Panel #1----#
 ser_Pin = 18
@@ -34,7 +34,7 @@ FlipDot_Panels[1] = FlipDot_Controller_Class.FlipDot_Controller_Class(1, onRows,
 
 registers=[0]*numOfRegisterPins
 
-def displayScroller():
+def multiPanel():
 	for FlipDot_Panel in FlipDot_Panels:
 		FlipDot_Panel.allDots(1)
 		FlipDot_Panel.allDots(0)
@@ -81,5 +81,5 @@ def worker(panelNumber,panelDisplay,columns_offset_total):
 
 
 
-displayScroller()
+multiPanel()
 FlipDot_Controller.deInitialize()
