ba3f9258d5bece6951366b082c52dda9f87c68d4
Yaniv Alfasy <yanivalfasy@Yanivs-MBP.lan>
2018-01-06 13:04:43 -0800

    Updating all files for cleanpup of old pin layouts.

--- Python_Controller/FlipDotTest_BitcoinPrice.py
+++ Python_Controller/FlipDotTest_BitcoinPrice.py
@@ -3,18 +3,16 @@ import requests, json
 from time import sleep
 
 #----Set up controller to understand PCB design and Microcontroller layout----#
-onRows = [11,19,16,20,23,21,22] #[0,1,2,3,4,5,6] #[11,19,16,20,23,21,22]
-offRows = [53,61,56,60,57,59,58] #[8,9,10,11,12,13,14]
-onColumns = [54,52,51,50,49,48,47,46,45,55,62,63,64,65,66,67,68,69,70,71,44,43,42,41,40,76,75,74,73,72] #[31,30,29,28,27,24,25,26,72,40,41,42,43,44,45,46,47,56,57,58,73,74,75,76,77,63,62,61,60,59]
-offColumns = [10,12,13,14,15,8,1,2,3,9,18,17,24,31,30,29,28,27,26,25,4,5,6,7,0,36,37,38,39,32] #[23,22,21,20,19,16,17,18,64,32,33,34,35,36,37,38,39,48,49,50,65,66,67,68,69,55,54,53,52,51]
+onRows = [11,19,16,20,23,21,22] 
+offRows = [53,61,56,60,57,59,58] 
+onColumns = [54,52,51,50,49,48,47,46,45,55,62,63,64,65,66,67,68,69,70,71,44,43,42,41,40,76,75,74,73,72] 
+offColumns = [10,12,13,14,15,8,1,2,3,9,18,17,24,31,30,29,28,27,26,25,4,5,6,7,0,36,37,38,39,32] 
 numOfRegisterPins = 10 * 8 
 ser_Pin = 18
 rclk_Pin = 23
 srclk_Pin = 24
 
 FlipDot_Controller = FlipDot_Controller_Class.FlipDot_Controller_Class(1, onRows, offRows, onColumns, offColumns, numOfRegisterPins, ser_Pin, rclk_Pin, srclk_Pin) 
-
-registers=[0]*numOfRegisterPins
 
 def getBitcoinPrice():
     URL = 'https://www.bitstamp.net/api/ticker/'
@@ -26,7 +24,7 @@ def getBitcoinPrice():
         print "Error querying Bitstamp API"
 
 def displayBitcoin():
-        #--display bitcoin price--#
+    #--display bitcoin price--#
 	btcprice = 0
 	try:
 		while True:
--- Python_Controller/FlipDotTest_MultiPanel.py
+++ Python_Controller/FlipDotTest_MultiPanel.py
@@ -8,10 +8,10 @@ logging.basicConfig(level=logging.DEBUG,
                     )
 
 #----Set up controller to understand PCB design and Microcontroller layout----#
-onRows = [11,19,16,20,23,21,22] #[0,1,2,3,4,5,6] #[11,19,16,20,23,21,22]
-offRows = [53,61,56,60,57,59,58] #[8,9,10,11,12,13,14]
-onColumns = [54,52,51,50,49,48,47,46,45,55,62,63,64,65,66,67,68,69,70,71,44,43,42,41,40,76,75,74,73,72] #[31,30,29,28,27,24,25,26,72,40,41,42,43,44,45,46,47,56,57,58,73,74,75,76,77,63,62,61,60,59]
-offColumns = [10,12,13,14,15,8,1,2,3,9,18,17,24,31,30,29,28,27,26,25,4,5,6,7,0,36,37,38,39,32] #[23,22,21,20,19,16,17,18,64,32,33,34,35,36,37,38,39,48,49,50,65,66,67,68,69,55,54,53,52,51]
+onRows = [11,19,16,20,23,21,22]
+offRows = [53,61,56,60,57,59,58]
+onColumns = [54,52,51,50,49,48,47,46,45,55,62,63,64,65,66,67,68,69,70,71,44,43,42,41,40,76,75,74,73,72]
+offColumns = [10,12,13,14,15,8,1,2,3,9,18,17,24,31,30,29,28,27,26,25,4,5,6,7,0,36,37,38,39,32]
 numOfRegisterPins = 10 * 8 
 FlipDot_Panels = [0 for i in range(3)]
 
@@ -31,26 +31,30 @@ rclk_Pin = 6
 srclk_Pin = 13
 FlipDot_Panels[2] = FlipDot_Controller_Class.FlipDot_Controller_Class(1, onRows, offRows, onColumns, offColumns, numOfRegisterPins, ser_Pin, rclk_Pin, srclk_Pin) 
 
-def multiPanel():
+def multiPanel(scroll_text,character_offset,scroll_text):
+	#----Do Initial Clearing----#
 	for FlipDot_Panel in FlipDot_Panels:
+		FlipDot_Panel.flipDelay = scroll_speed
 		FlipDot_Panel.allDots(1)
 		FlipDot_Panel.allDots(0)
-		sleep(.01)
-	displayText = "KELEIGH SUCKS! "
+	
+	#----Start Scroller---#
 	columns_offset = 0
-	columns_at_a_time = 6
+	columns_at_a_time = character_offset
 	columns_each_character = 6
 	try:
 		print "Press Ctrl+C to Stop Test."
 		while True:
-			message = displayText+displayText
+			message = scroll_text+" "+scroll_text
 			columns_offset_total = columns_offset*columns_at_a_time
-			n=0
+			panelnum=0
+			
 			for FlipDot_Panel in FlipDot_Panels:
-				c=n*5
+				c=panelnum*5
 				nMessage = message[c:]
-				n=n+1
+				panelnum=panelnum+1
 				FlipDot_Panel.updateDisplay(nMessage,columns_offset_total)
+
 			if columns_offset>=(len(displayText)*columns_each_character)/columns_at_a_time:
 				columns_offset=1
 			else:
@@ -60,13 +64,11 @@ def multiPanel():
 			FlipDot_Panel.deInitialize
 		pass
 
-def worker(panelNumber,panelDisplay,columns_offset_total):
-	logging.debug('Starting Panel #:'+str(panelNumber))
-	FlipDot_Panels[panelNumber].updateDisplay(panelDisplay,columns_offset_total)
-	logging.debug('Exiting Panel #:'+str(panelNumber))
-	return
+scroll_text = raw_input("Scroll Text? ")
+character_offset = int(raw_input("Character Offset? "))
+scroll_speed = float(raw_input("Speed? "))
+multiPanel(scroll_text, character_offset, scroll_speed)
 
-multiPanel()
 for FlipDot_Panel in FlipDot_Panels:
 	FlipDot_Panel.deInitialize
 	sleep(.01)
