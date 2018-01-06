8b0703b3a3b192727f5ee336d18ac080f565ff9a
Yaniv Alfasy <yanivalfasy@Yanivs-MBP.lan>
2018-01-06 11:37:53 -0800

    Updating FlipDot for new voltage and multipanel.

--- Python_Controller/FlipDotTest_MultiPanelThreaded.py
+++ Python_Controller/FlipDotTest_MultiPanelThreaded.py
@@ -1,6 +1,7 @@
 import FlipDot_Controller_Class
 from time import sleep
 import threading
+from multiprocessing import Process
 import logging
 
 logging.basicConfig(level=logging.DEBUG,
@@ -14,6 +15,7 @@ onColumns = [54,52,51,50,49,48,47,46,45,55,62,63,64,65,66,67,68,69,70,71,44,43,4
 offColumns = [10,12,13,14,15,8,1,2,3,9,18,17,24,31,30,29,28,27,26,25,4,5,6,7,0,36,37,38,39,32] #[23,22,21,20,19,16,17,18,64,32,33,34,35,36,37,38,39,48,49,50,65,66,67,68,69,55,54,53,52,51]
 numOfRegisterPins = 10 * 8 
 FlipDot_Panels = [0 for i in range(3)]
+p = [0 for i in range(3)]
 
 #----Panel #1----#
 ser_Pin = 18
@@ -35,18 +37,8 @@ def multiPanel():
 	#----Do Initial Clearing----#
 	panelnum = 0
 	for FlipDot_Panel in FlipDot_Panels:
-		t = threading.Thread(target=onOffer, kwargs={'panelNumber':panelnum})
-		panelnum = panelnum+1
-		logging.debug('starting %s', t.getName())
-		t.start
-	main_thread = threading.currentThread()
-		#FlipDot_Panel.allDots(1)
-		#FlipDot_Panel.allDots(0)
-	for t in threading.enumerate():
-		if t is main_thread:
-			continue
-		t.join()
-		logging.debug('joined %s', t.getName())
+		FlipDot_Panel.allDots(1)
+		FlipDot_Panel.allDots(0)
 
 	#----Start Scroller---#
 	displayText = "KELEIGH SUCKS! "
@@ -62,16 +54,15 @@ def multiPanel():
 			for FlipDot_Panel in FlipDot_Panels:
 				c=panelnum*5
 				nMessage = message[c:]
-				t = threading.Thread(target=flipScroller, kwargs={'panelNumber':panelnum,'panelDisplay':nMessage,'columns_offset_total':columns_offset_total})
+				p[panelnum] = Process(target=flipScroller, kwargs={'panelNumber':panelnum,'panelDisplay':nMessage,'columns_offset_total':columns_offset_total})
+				p[panelnum].start()
 				panelnum=panelnum+1
-				logging.debug('starting %s', t.getName())
-				t.start()
-			main_thread = threading.currentThread()
-			for t in threading.enumerate():
-				if t is main_thread:
-					continue
-				t.join()
-				logging.debug('joined %s', t.getName())
+
+			panelnum=0
+			for FlipDot_Panel in FlipDot_Panels:
+				p[panelnum].join()
+				panelnum=panelnum+1
+
 			if columns_offset>=(len(displayText)*columns_each_character)/columns_at_a_time:
 				columns_offset=1
 			else:
@@ -79,8 +70,6 @@ def multiPanel():
 	except KeyboardInterrupt:
 		for FlipDot_Panel in FlipDot_Panels:
 			FlipDot_Panel.deInitialize
-		logging.debug('cancelling %s', t.getName())
-		t.cancel()
 		pass
 
 def flipScroller(panelNumber,panelDisplay,columns_offset_total):
