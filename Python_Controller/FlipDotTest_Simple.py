d82698761ce977816a337997eae91aee8be3c927
Yaniv Alfasy <yanivalfasy@Yanivs-MBP.lan>
2018-01-05 22:44:04 -0800

    Updating FlipDot for new voltage and multipanel.

--- Python_Controller/FlipDotTest_Simple.py
+++ Python_Controller/FlipDotTest_Simple.py
@@ -19,7 +19,7 @@ def simpleTest():
 		print "Press Ctrl+C to Stop Test."
 		while True:
 			for x in range(1,7):
-				FlipDot_Controller.simpleTest(1, 20)
+				FlipDot_Controller.simpleTest(x, 20)
 	except KeyboardInterrupt:
 		FlipDot_Controller.deInitialize()
 		pass
